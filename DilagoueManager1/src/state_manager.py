c8a6f60c5ffa7035055a6f0e8bd4e262db39b27f
Atahan Çaldır <45735539+atahancaldir@users.noreply.github.com>
2021-07-13 10:24:46 +0300

    Implemented Sqlite3 for the knowledge base

--- DilagoueManager1/src/state_manager.py
+++ DilagoueManager1/src/state_manager.py
@@ -4,6 +4,9 @@ import random
 import sys
 import nlg
 
+import sqlite3
+import os
+
 import smach
 import smach_ros
 
@@ -11,7 +14,7 @@ import rospy
 from dialogue_manager.srv import *
 
 rospy.init_node('state_manager', anonymous=True)
-
+# arguments for the knowledge base
 rospy.wait_for_service('speech_recognizer_srv')
 rospy.wait_for_service('text_to_speech_srv')
 
@@ -26,7 +29,7 @@ class PredProcess(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
                         outcomes=['hello', 'day', 'goodbye'],
-                        input_keys=['kb_args'],
+                        input_keys=['kb_args', 'db_con', 'db_cursor'],
                         output_keys=['kb_args', 'status'])
 
         self.first_time = True
@@ -39,7 +42,12 @@ class PredProcess(smach.State):
             tts_proxy(sentence)
             print 'Pepper:', sentence
 
-            userdata.kb_args['name'] = raw_input('Enter your first name: ')
+            name = raw_input('Enter your first name: ')
+
+            userdata.kb_args['name'] = name
+            userdata.db_cursor.execute('INSERT INTO userdata VALUES(?,?,?)', (name, '', 0))
+            userdata.db_con.commit()
+
             self.first_time = False
 
             return 'hello'
@@ -74,7 +82,7 @@ class Hello(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
                              outcomes=['predprocess', 'goodbye'],
-                             input_keys=['kb_args'],
+                             input_keys=['kb_args', 'db_con', 'db_cursor'],
                              output_keys=['status', 'kb_args'])
 
     def execute(self, userdata):
@@ -100,7 +108,10 @@ class Hello(smach.State):
                 sentence = nlg.dialogue['info_saved'][0].format(userdata.kb_args['name'])
                 tts_proxy(sentence)
                 print 'Pepper:', sentence
+
                 userdata.kb_args['ID'] = speech_text.outp
+                userdata.db_cursor.execute('UPDATE userdata SET id = ? WHERE name = ? AND id = ?', (speech_text.outp, userdata.kb_args['name'], ''))
+                userdata.db_con.commit()
 
                 return 'predprocess'
 
@@ -111,7 +122,7 @@ class Day(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
                              outcomes=['predprocess', 'goodbye'],
-                             input_keys=['kb_args'],
+                             input_keys=['kb_args', 'db_con', 'db_cursor'],
                              output_keys=['status', 'kb_args'])
 
     def execute(self, userdata):
@@ -131,7 +142,10 @@ class Day(smach.State):
                 sentence = nlg.dialogue['day_rates'][0].format(userdata.kb_args['name'])
                 tts_proxy(sentence)
                 print 'Pepper:', sentence
+
                 userdata.kb_args['day_rate'] = '1'
+                userdata.db_cursor.execute('UPDATE userdata SET day_rate = ? WHERE name = ? AND id = ?', (1, userdata.kb_args['name'], userdata.kb_args['ID']))
+                userdata.db_con.commit()
                 
                 return 'predprocess'
 
@@ -139,7 +153,10 @@ class Day(smach.State):
                 sentence = nlg.dialogue['day_rates'][1].format(userdata.kb_args['name'])
                 tts_proxy(sentence)
                 print 'Pepper:', sentence
+
                 userdata.kb_args['day_rate'] = '2'
+                userdata.db_cursor.execute('UPDATE userdata SET day_rate = ? WHERE name = ? AND id = ?', (2, userdata.kb_args['name'], userdata.kb_args['ID']))
+                userdata.db_con.commit()
                 
                 return 'predprocess'
 
@@ -147,7 +164,10 @@ class Day(smach.State):
                 sentence = nlg.dialogue['day_rates'][2].format(userdata.kb_args['name'])
                 tts_proxy(sentence)
                 print 'Pepper:', sentence
+
                 userdata.kb_args['day_rate'] = '3'
+                userdata.db_cursor.execute('UPDATE userdata SET day_rate = ? WHERE name = ? AND id = ?', (3, userdata.kb_args['name'], userdata.kb_args['ID']))
+                userdata.db_con.commit()
                 
                 return 'predprocess'
 
@@ -184,6 +204,13 @@ class Goodbye(smach.State):
 def main():
 
     try:
+        con = sqlite3.connect('knowledge_base.db')
+        cursor = con.cursor()
+
+        cursor.execute('CREATE TABLE IF NOT EXISTS userdata(name TEXT, id TEXT, day_rate INTEGER)')
+        con.commit()
+
+        rospy.loginfo('Knowledge Base DB is saved to ' + os.getcwd())
 
         sm = smach.StateMachine(outcomes=['outcome_termination'])
 
@@ -195,25 +222,34 @@ def main():
                                   'ID': '0',
                                   'day_rate': '0'}
 
+        sm.userdata.sm_db_con = con
+        sm.userdata.sm_db_cursor = cursor
+
         with sm:
             smach.StateMachine.add('PREDPROCESS', PredProcess(),
                                    transitions={'hello':'HELLO',
                                                 'day':'DAY',
                                                 'goodbye':'GOODBYE'},
                                    remapping={'status':'sm_status',
-                                              'kb_args':'sm_kb_args'})
+                                              'kb_args':'sm_kb_args',
+                                              'db_con':'sm_db_con',
+                                              'db_cursor':'sm_db_cursor'})
             
             smach.StateMachine.add('HELLO', Hello(),
                                    transitions={'predprocess':'PREDPROCESS',
                                                 'goodbye':'GOODBYE'},
                                    remapping={'status':'sm_status',
-                                              'kb_args':'sm_kb_args'})
+                                              'kb_args':'sm_kb_args',
+                                              'db_con':'sm_db_con',
+                                              'db_cursor':'sm_db_cursor'})
             
             smach.StateMachine.add('DAY', Day(),
                                    transitions={'predprocess':'PREDPROCESS',
                                                 'goodbye':'GOODBYE'},
                                    remapping={'kb_args':'sm_kb_args',
-                                              'status':'sm_status'})
+                                              'status':'sm_status',
+                                              'db_con':'sm_db_con',
+                                              'db_cursor':'sm_db_cursor'})
 
             smach.StateMachine.add('GOODBYE', Goodbye(),
                                    transitions={'terminate':'outcome_termination'},
@@ -226,6 +262,9 @@ def main():
 
     except:
         print('Unexpected error:', sys.exc_info()[0])
+
+    finally:
+        con.close()
         
 
 if __name__ == '__main__':
